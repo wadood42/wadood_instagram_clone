843559ee9eb9a5a835a65bb4742112cc0f95b55f
Abdul <wbehzad12@gmail.com>
2021-01-28 23:16:22 -0800

    made it better

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,10 +10,6 @@ class UsersController < ApplicationController
     def new
         @user = User.new
     end
-
-    def show
-        
-    end
     # def create
         # @user = User.new(user_params)
 
